fc1f8e1bfca17510b202015a12aa20800431f30c
Son Dam <snmquangams@gmail.com>
2022-11-30 09:39:05 -0500

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -10,9 +10,14 @@ from requests.auth import HTTPBasicAuth
 def get_request(url, **kwargs):
     print(kwargs)
     print("GET from {} ".format(url))
+    apikey = kwargs.get("apikey")
     try:
         # Call get method of requests library with URL and parameters
-        response = requests.get(url, headers={'Content-Type': 'application/json'},
+        if apikey:
+            response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                    auth=HTTPBasicAuth('apikey', apikey), params=kwargs)
+        else: 
+            response = requests.get(url, headers={'Content-Type': 'application/json'},
                                     params=kwargs)
     except:
         # If any error occurs
