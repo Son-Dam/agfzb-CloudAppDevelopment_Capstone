a10ec216bb4c0e2e06f75b47ba3e761de3614a97
Son Dam <snmquangams@gmail.com>
2022-12-04 15:26:52 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -109,7 +109,15 @@ def get_dealerships(request,**kwargs):
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
-
+def get_dealer_details(request, dealer_id):
+    if request.method == "GET":
+        url = "https://us-east.functions.appdomain.cloud/api/v1/web/Son%20Dam_djangoserver-space/dealership-package/get-review"
+        # Get dealers from the URL
+        dealerships = get_dealer_reviews_from_cf(url, dealership = dealer_id)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
