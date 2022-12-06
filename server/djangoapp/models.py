4d8fbf98de569de4a9ba9ad14f3a478915e6988d
Son Dam <snmquangams@gmail.com>
2022-12-06 04:19:11 -0500

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -83,23 +83,14 @@ class DealerReview:
             name, 
             purchase, 
             review, 
-            purchase_date, 
-            car_make, 
-            car_model, 
-            car_year, 
-            sentiment,
-            id):
+            sentiment):
         
         self.dealership = dealership
         self.name = name
         self.purchase = purchase
         self.review = review
-        self.purchase_date= purchase_date
-        self.car_make = car_make
-        self.car_model = car_model
-        self.car_year = car_year
         self.sentiment = sentiment
-        self.id = id
+        
     def __str__(self):
         return f"Dealerhip name: {self.name }" 
                 
