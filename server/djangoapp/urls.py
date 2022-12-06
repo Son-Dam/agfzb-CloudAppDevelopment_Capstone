e1aac10769fbd2b17f70e33d6e318175f074472c
Son Dam <snmquangams@gmail.com>
2022-12-05 22:49:17 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path(route='', view=views.get_dealerships, name='index'),
     path(route='<str:st>', view=views.get_dealerships, name='index_st'),
     path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
-
+    path('review/<int:dealer_id>/', views.add_review, name='add_review'),
     # path for dealer reviews view
 
     # path for add a review view
